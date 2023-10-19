ab114b74306b4b6223318b649d1fece600c57da8
Vladimír Chlup <jajik@chlup.net>
2023-10-19 19:35:32 +0200

    Add Num instance and a small commentary

--- phantom_types.hs
+++ phantom_types.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
+
 data Degree = Degree
             deriving (Show, Eq)
 
@@ -7,8 +9,8 @@ data Radian = Radian
 -- See, the `unit` type is not used anywhere on the right side
 -- It is used as a tag so you can't then compare Angles in radians
 -- to Angles in degrees.
-data Angle unit = Angle { getVal :: Double }
-                deriving (Show, Eq)
+newtype Angle unit = Angle { getVal :: Double }
+                deriving (Show, Eq, Num)
 
 
 rads :: Double -> Angle Radian
@@ -24,5 +26,17 @@ radsToDeg (Angle r) = Angle (r * 180/pi)
 degsToRad :: Angle Degree -> Angle Radian
 degsToRad (Angle d) = Angle (pi/180 * d)
 
-
+-- We now can use degrees and radians and thank to derived Num instance,
+-- the basic arithmetic works as well.
+--
+-- degs 30 + degs 60 ~> Angle { getVal = 90 }
+-- rads (pi/2) + rads (pi/2) ~> Angle { getVal = pi }
+--
+-- BUT
+--
+-- degs 30 + rads pi
+--
+-- Will throw an error!
+--
+-- But it can be even better...
 
